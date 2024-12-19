b7958a658ccb7938d754602fb8d0cec9cc64b435
javi <57828409+partiaso@users.noreply.github.com>
2024-12-19 10:55:51 +0100

    add prod and dev environments to webpack

--- restaurant-page/webpack.common.js
+++ restaurant-page/webpack.common.js
@@ -2,8 +2,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports =  {
-    mode: "development",
-    entry: "./src/index.js",
+    entry: { 
+        app: "./src/index.js",
+    },
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
